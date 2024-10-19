98e3e7eeab9321e18f4abf70eedb9e7aa07ac070
pdJeeves <pdjeeves@zoho.com>
2024-10-19 07:40:04 -0500

    Sat Oct 19 07:39:34 AM CDT 2024

--- src/asdocumenter.h
+++ src/asdocumenter.h
@@ -15,7 +15,7 @@ class asIScriptEngine;
 class asDocumenter
 {
 public:
-typedef std::map<intptr_t, const char *> mapping;
+typedef std::map<int, const char *> mapping;
 	static int insensitiveStrncmpAscii(const char * a, const char * b, int length);
 
 	asDocumenter() = default;
@@ -23,86 +23,90 @@ typedef std::map<intptr_t, const char *> mapping;
 
 	void clear();
 
-	inline const char * GetInterface (intptr_t typeId) const { return ReadTypeId(typeId); }
-	inline const char * GetObjectType(intptr_t typeId) const { return ReadTypeId(typeId); }
-	inline const char * GetTypedef   (intptr_t typeId) const { return ReadTypeId(typeId); }
-	inline const char * GetFuncDef   (intptr_t typeId) const { return ReadTypeId(typeId); }
-	inline const char * GetEnum      (intptr_t typeId) const { return ReadTypeId(typeId); }
+	inline const char * GetInterface (int typeId) const { return ReadTypeId(typeId); }
+	inline const char * GetObjectType(int typeId) const { return ReadTypeId(typeId); }
+	inline const char * GetTypedef   (int typeId) const { return ReadTypeId(typeId); }
+	inline const char * GetFuncDef   (int typeId) const { return ReadTypeId(typeId); }
+	inline const char * GetEnum      (int typeId) const { return ReadTypeId(typeId); }
 
-	inline const char * GetObjectMethod   (intptr_t funcId) const { return ReadFunction(funcId); }
-	inline const char * GetGlobalFunction (intptr_t funcId) const { return ReadFunction(funcId); }
-	inline const char * GetObjectBehaviour(intptr_t funcId) const { return ReadFunction(funcId); }
+	inline const char * GetObjectMethod   (int funcId) const { return ReadFunction(funcId); }
+	inline const char * GetGlobalFunction (int funcId) const { return ReadFunction(funcId); }
+	inline const char * GetObjectBehaviour(int funcId) const { return ReadFunction(funcId); }
 
-	inline const char * GetEnumValue     (intptr_t typeId, intptr_t value)  const { return ReadOffset(typeId, value ); }
-	inline const char * GetObjectProperty(intptr_t typeId, intptr_t offset) const { return ReadOffset(typeId, offset); }
-	inline const char * GetGlobalProperty(intptr_t offset)                  const { return ReadOffset(0,      offset); }
+	inline const char * GetEnumValue     (int typeId, int value)  const { return ReadOffset(typeId, value ); }
+	inline const char * GetObjectProperty(int typeId, int offset) const { return ReadOffset(typeId, offset); }
+	inline const char * GetGlobalProperty(int offset)                  const { return ReadOffset(0,      offset); }
 
-	inline const char * GetFunctionOverride(intptr_t offset)                const { return ReadOverrideFunction(offset); }
+	inline const char * GetFunctionOverride(int offset)                const { return ReadOverrideFunction(offset); }
 
-	inline void RegisterInterface (intptr_t typeId, const char * description) { WriteTypeId(typeId, description); }
-	inline void RegisterObjectType(intptr_t typeId, const char * description) { WriteTypeId(typeId, description); }
-	inline void RegisterTypedef   (intptr_t typeId, const char * description) { WriteTypeId(typeId, description); }
-	inline void RegisterFuncDef   (intptr_t typeId, const char * description) { WriteTypeId(typeId, description); }
-	inline void RegisterEnum      (intptr_t typeId, const char * description) { WriteTypeId(typeId, description); }
+	inline void RegisterInheritance (int typeId, int subTypeId) { m_inheritance.push_back({typeId, subTypeId}); }
+	inline void RegisterInterface(int typeId, const char * description) { WriteTypeId(typeId, description); }
 
-	inline void RegisterObjectMethod   (intptr_t funcId, const char * description) { WriteFunction(funcId, description); }
-	inline void RegisterGlobalFunction (intptr_t funcId, const char * description) { WriteFunction(funcId, description); }
-	inline void RegisterObjectBehaviour(intptr_t funcId, const char * description) { WriteFunction(funcId, description); }
+	inline void RegisterObjectType(int typeId, const char * description) { WriteTypeId(typeId, description); }
+	inline void RegisterTypedef   (int typeId, const char * description) { WriteTypeId(typeId, description); }
+	inline void RegisterFuncDef   (int typeId, const char * description) { WriteTypeId(typeId, description); }
+	inline void RegisterEnum      (int typeId, const char * description) { WriteTypeId(typeId, description); }
 
-	inline void OverrideObjectMethod   (intptr_t funcId, const char * description) { WriteOverrideFunction(funcId, description); }
-	inline void OverrideGlobalFunction (intptr_t funcId, const char * description) { WriteOverrideFunction(funcId, description); }
+	inline void RegisterObjectMethod   (int funcId, const char * description) { WriteFunction(funcId, description); }
+	inline void RegisterGlobalFunction (int funcId, const char * description) { WriteFunction(funcId, description); }
+	inline void RegisterObjectBehaviour(int funcId, const char * description) { WriteFunction(funcId, description); }
 
-	inline void RegisterEnumValue     (intptr_t typeId, int value,       const char * description) { WriteOffset(typeId, value , description); }
-	inline void RegisterObjectProperty(intptr_t typeId, intptr_t offset, const char * description) { WriteOffset(typeId, offset, description); }
-	inline void RegisterGlobalProperty(intptr_t offset,                  const char * description) { WriteOffset(0,      offset, description); }
+	inline void OverrideObjectMethod   (int funcId, const char * description) { WriteOverrideFunction(funcId, description); }
+	inline void OverrideGlobalFunction (int funcId, const char * description) { WriteOverrideFunction(funcId, description); }
 
-	inline void AddToModule(intptr_t typeId, const char * _module) { m_modules.push_back({typeId, _module}); }
-	inline void AssociateGlobalFunctions(intptr_t typeId, intptr_t first_funcId, intptr_t lastFuncId) { m_globals.push_back({typeId, first_funcId, lastFuncId}); }
-	void RegisterSubtype(intptr_t typeId, intptr_t subtypeId);
+	inline void RegisterEnumValue     (int typeId, int value,       const char * description) { WriteOffset(typeId, value , description); }
+	inline void RegisterObjectProperty(int typeId, int offset, const char * description) { WriteOffset(typeId, offset, description); }
+	inline void RegisterGlobalProperty(int offset,                  const char * description) { WriteOffset(0,      offset, description); }
+
+	inline void AddToModule(int typeId, const char * _module) { m_modules.push_back({typeId, _module}); }
+	inline void AssociateGlobalFunctions(int typeId, int first_funcId, int lastFuncId) { m_globals.push_back({typeId, first_funcId, lastFuncId}); }
+	void RegisterSubtype(int typeId, int subtypeId);
 
 	inline void AddPage(const char * title, const char * desc) { m_miscPages.push_back({title, desc}); }
 
 	std::vector<const char *> GetModules();
-	std::vector<intptr_t> GetModuleContents(const char *);
+	std::vector<int> GetModuleContents(const char *);
 
-	std::vector<intptr_t> GetSubTypes(intptr_t typeId);
-	const char *		  GetModule(intptr_t typeId);
-	std::vector<std::pair<intptr_t, intptr_t>> GetGlobals(intptr_t);
-	bool				  IsGlobalAssociated(intptr_t);
-	int64_t				  GetParentType(intptr_t);
+	std::vector<int> GetSubTypes(int typeId);
+	const char *		  GetModule(int typeId);
+	std::vector<std::pair<int, int>> GetGlobals(int);
+	bool				  IsGlobalAssociated(int);
+	int64_t				  GetParentType(int);
 
 	void AddSpecialNamespace(const char * it) { m_modules.push_back({0, it}); }
 	bool IsSpecialNamespace(const char * c) const;
 
-	bool SilenceFunction(intptr_t funcId) const;
+	bool SilenceFunction(int funcId) const;
 
 private:
-	const char * ReadTypeId(intptr_t typeId) const;
-	void         WriteTypeId(intptr_t typeId, const char * description);
+	const char * ReadTypeId(int typeId) const;
+	void         WriteTypeId(int typeId, const char * description);
 
-	const char * ReadFunction(intptr_t funcId) const;
-	void         WriteFunction(intptr_t funcId, const char * description);
+	const char * ReadFunction(int funcId) const;
+	void         WriteFunction(int funcId, const char * description);
 
-	const char * ReadOverrideFunction(intptr_t funcId) const;
-	void         WriteOverrideFunction(intptr_t funcId, const char * description);
+	const char * ReadOverrideFunction(int funcId) const;
+	void         WriteOverrideFunction(int funcId, const char * description);
 
-	const char * ReadOffset(intptr_t typeId, intptr_t offset) const;
-	void         WriteOffset(intptr_t typeId, intptr_t offset, const char * description);
+	const char * ReadOffset(int typeId, int offset) const;
+	void         WriteOffset(int typeId, int offset, const char * description);
 
 	std::vector<const char *>   m_funcDscr;
 	std::vector<const char *>   m_funcOverride;
 	mapping                     m_typeDscr;
 	mapping                     m_offsetDscr;
-	std::map<intptr_t, mapping> m_propertyDscr;
+	std::map<int, mapping> m_propertyDscr;
 
-	std::vector<std::pair<intptr_t, const char *>>         m_modules;
-	std::vector<std::pair<intptr_t, intptr_t>>             m_subTypes;
-	std::vector<std::tuple<intptr_t, intptr_t, intptr_t> > m_globals;
+	std::vector<std::pair<int, const char *>>         m_modules;
+	std::vector<std::pair<int, int>>             m_subTypes;
+	std::vector<std::pair<int, int>>             m_inheritance;
+	std::vector<std::tuple<int, int, int> > m_globals;
 
 	std::vector<std::pair<const char *, const char *>>		m_miscPages;
-//	mutable std::map<intptr_t, mapping>::iterator last_property{m_propertyDscr.end()};
+//	mutable std::map<int, mapping>::iterator last_property{m_propertyDscr.end()};
 };
 
+#define docInhertiance(x, y)  doc->RegisterInheritance	(x, y )
 #define docInterface(x, y)    doc->RegisterInterface	(x, y )
 #define docObjectType(x, y)   doc->RegisterObjectType	(x, y )
 #define docTypedef(x, y)      doc->RegisterTypedef		(x, y )
@@ -134,6 +138,7 @@ class asDocumenter
 {
 };
 
+#define docInheritance(x, y)  (void)doc; (void)x
 #define docInterface(x, y)    (void)doc; (void)x
 #define docObjectType(x, y)   (void)doc; (void)x
 #define docTypedef(x, y)      (void)doc; (void)x
